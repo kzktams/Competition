fb3933a0d2dfdd2420750ef8f772b621b4e29ac1
kzktams <kzktams@gmail.com>
2024-03-12 11:18:24 +0100

    asfdasd

--- Competition/Models/Athlete.cs
+++ Competition/Models/Athlete.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,60 @@ using System.Threading.Tasks;
 
 namespace Competition.Models
 {
-    internal class Athlete
+    public class Athletee : ObservableObject
     {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private double personalBest;
+
+        public double PersonalBest
+        {
+            get { return personalBest; }
+            set { SetProperty(ref personalBest, value); }
+        }
+
+        private double yearBest;
+
+        public double YearBest
+        {
+            get { return yearBest; }
+            set { SetProperty(ref yearBest, value); }
+        }
+
+        private bool compLegal;
+
+        public bool CompLegal
+        {
+            get { return compLegal; }
+            set { compLegal = value; }
+        }
+
+
+
+        private string team;
+
+        public string Team
+        {
+            get { return team; }
+            set { SetProperty(ref team, value); }
+        }
+
+
+        public double MarketValue
+        {
+            get
+            {
+                return PersonalBest * YearBest;
+            }
+
+        }
+
+
     }
 }
